e527aa6f2fe1d2af2cdab893641a2863e1384f06
dobriddd <dobromirdenev212@gmail.com>
2025-05-13 22:59:35 +0300

    A minor change

--- assignment_manager.h
+++ assignment_manager.h
@@ -5,7 +5,7 @@
 
 class AssignmentManager {
 public:
-    static int getNextAssignmentId();  // Нов метод
+    static int getNextAssignmentId();
     static void createAssignment(int courseId, const std::string& description);
     static void submitAssignment(int assignmentId, int studentId, const std::string& answer);
     static void gradeAssignment(int assignmentId, int studentId, int grade);
